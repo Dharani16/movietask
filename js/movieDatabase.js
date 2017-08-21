7f2e72d535a9ab629e7e0af8073c29175cb4c602
Dharani <rdharani1606@gmail.com>
2017-08-21 09:55:48 +0530

    code arrangement

--- js/movieDatabase.js
+++ js/movieDatabase.js
@@ -1,16 +1,14 @@
+
+// Display movie list
 $(document).ready(function(){
 	$.getJSON("http://api.themoviedb.org/3/discover/movie?api_key=22d8568621673d1336e8d0d1fb253821",function(data){
 		$.each(data.results,function(key,value){
-			$("#movieList").append('<li><a href="#" onClick="callMovie('+value.id+'); return false;">' + value.title + '</a></li>');
-			//\'' + valuationId + '\',\'' + user + '\'
-			//to display movie posterPath -> var image = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
-			//$("#movieList").append('<li><a href="#" onClick="callMovie(\'' + value.id + '\',\'' + value.title +  '\',\'' + value.overview +  '\',\'' + value.poster_path +  '\',); return false;">' + value.title + '</a></li>');
+			$("#movieList").append('<li><a href="#" onClick="callMovie('+value.id+'); return false;">' + value.title + '</a></li>');			
 		});
 	});
 });
 
 function callMovie(movieId) {
-	//$('#testing').animate({width: 500, marginLeft: 0}, {duration: 1000});
 	//https://api.themoviedb.org/3/movie/550?api_key=22d8568621673d1336e8d0d1fb253821
 	$("#contentLeftPart").css("width","50%");
 	$("#contentRightPart").css("width","50%");
@@ -32,9 +30,40 @@ function callMovie(movieId) {
 		$("#movieReleaseTitle").html("Released Date");
 		$("#movieReleaseId").html(data.release_date);
 
-		// $.each(data.genres,function(key,value){			
-		// 	console.log("Genres : "+value.name+",");
-		// });
-
 	});				
 }
+
+// script for about us
+$(document).ready(function(){
+	$("#aboutId").click(function(){
+		$.getJSON("json/aboutJson.json",function(data){
+			$.each(data.about,function(key,value){
+				$("#homeSectionPart .row").html(aboutUsWrapper(value.title, value.content));
+			});
+		});	
+	});	
+});
+
+function aboutUsWrapper(title, content) {
+	var output = '<div class="about-us-section"> <span class="title">' + title + ' </br> </span> <span class="content">' + content + '</span> </div>';
+	return output;
+}
+
+// contact us page
+$(document).ready(function(){
+  	$("#contactId").click(function(){
+	  $("div#homePartDummy").html( 
+		    $("<form/>",{action:'#', method:'#'}).append(   
+		    $("<input/>", {type:'text', id:'vname', name:'name', placeholder:'Your Name'}), 
+		    $("<br/>"),
+		    $("<br/>"),
+		    $("<input/>", {type:'text', id:'vemail', name:'email', placeholder:'Your Email'}),
+		    $("<br/>"),
+		    $("<br/>"),
+		    $("<textarea/>", {rows:'5px', cols:'27px', type:'text', id:'vmsg', name:'msg', placeholder:'Message'}),
+		    $("<br/>"),
+		    $("<br/>"),
+		    $("<input/>", {type:'submit', id:'submit', value:'Submit'}))
+	    )  
+	}); 
+});
